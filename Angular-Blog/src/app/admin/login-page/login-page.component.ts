1b0cfd55ca11c2b6770006b2f4571efe1b1d715e
Lyubomir Lyubomirov <Ljubo6@abv.bg>
2022-09-24 15:35:58 +0300

    error handling

--- Angular-Blog/src/app/admin/login-page/login-page.component.ts
+++ Angular-Blog/src/app/admin/login-page/login-page.component.ts
@@ -13,7 +13,7 @@ export class LoginPageComponent implements OnInit {
   form!: FormGroup
   submitted = false
   constructor(
-    private auth: AuthService,
+    public auth: AuthService,
     private router: Router
     ) { }
 
@@ -39,10 +39,15 @@ export class LoginPageComponent implements OnInit {
       email: this.form.value.email,
       password: this.form.value.password
     }
-    this.auth.login(user).subscribe(() => {
-      this.form.reset()
-      this.router.navigate(['/admin','dashboard'])
-      this.submitted = false
+    this.auth.login(user).subscribe({
+      next: () => {
+        this.form.reset()
+        this.router.navigate(['/admin','dashboard'])
+        this.submitted = false
+      },
+      error: () => {
+        this.submitted = false
+      }
     })
 
   }
--- Angular-Blog/src/app/admin/shared/services/auth.service.ts
+++ Angular-Blog/src/app/admin/shared/services/auth.service.ts
@@ -1,37 +1,56 @@
 import {Injectable} from "@angular/core";
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpErrorResponse} from "@angular/common/http";
 import {FbAuthResponse, User} from "../../../shared/interfaces";
-import {Observable, tap} from "rxjs";
+import {catchError, Observable, ObservableInput, Subject, tap, throwError} from "rxjs";
 import {environment} from "../../../../environments/environment";
 
 @Injectable()
 export class AuthService{
+  public error$: Subject<string> = new Subject<string>()
   constructor(private http: HttpClient) {
   }
   get token():string {
-    const expDate = new Date(localStorage.getItem('fb-token-xp')!)
+    const expDate = new Date(localStorage.getItem('fb-token-exp')!)
     if(new Date() > expDate){
       this.logout()
       return null!
     }
     return localStorage.getItem('fb-token')!
+
   }
   login(user:User):Observable<any>{
     user.returnSecureToken = true
     return this.http.post(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${environment.apiKey}`,user)
       .pipe(
-        tap(this.setToken)
+        tap(this.setToken),
+        catchError(this.handleError.bind(this))
       )
   }
   logout(){
-    this.setToken(null)
+    this.setToken(null!)
   }
   isAuthenticated():boolean{
     return !!this.token
   }
-  private setToken(response: any){
+  private handleError(error: HttpErrorResponse): ObservableInput<any>{
+    const {message} = error.error.error
+    switch (message) {
+      case 'INVALID_EMAIL':
+        this.error$.next('Грешен email')
+        break
+      case 'INVALID_PASSWORD':
+        this.error$.next('Грешен password')
+        break
+      case 'EMAIL_NOT_FOUND':
+        this.error$.next('Няма такъв email')
+        break
+
+    }
+    return throwError(() => error)
+  }
+  public setToken(response: any){
     if(response) {
-      const expDate = new Date(new Date().getTime() + Number(response.expiresIn * 1000))
+      const expDate = new Date(new Date().getTime() + Number(response.expiresIn) * 1000)
       localStorage.setItem('fb-token', response.idToken)
       localStorage.setItem('fb-token-exp', expDate.toString())
     }else{
