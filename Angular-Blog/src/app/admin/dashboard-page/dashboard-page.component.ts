4543c1be3b259431a5d8c057656848456dee700f
Lyubomir Lyubomirov <Ljubo6@abv.bg>
2022-09-26 23:25:16 +0300

    extract list of posts

--- Angular-Blog/src/app/admin/dashboard-page/dashboard-page.component.ts
+++ Angular-Blog/src/app/admin/dashboard-page/dashboard-page.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {PostsService} from "../../shared/posts.service";
+import {Post} from "../../shared/interfaces";
+import {Subscription} from "rxjs";
 
 
 @Component({
@@ -6,11 +9,26 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './dashboard-page.component.html',
   styleUrls: ['./dashboard-page.component.scss']
 })
-export class DashboardPageComponent implements OnInit {
-
-  constructor() { }
+export class DashboardPageComponent implements OnInit, OnDestroy {
+  posts:Post[] = []
+  pSub!:Subscription
+  constructor(private postsService:PostsService) { }
 
   ngOnInit(): void {
+    this.pSub = this.postsService.getAll().subscribe({
+      next:(posts) => {
+        this.posts = posts
+      }
+    })
+  }
+  ngOnDestroy() {
+    if(this.pSub){
+      this.pSub.unsubscribe()
+    }
   }
 
+
+  remove(id: string) {
+
+  }
 }
