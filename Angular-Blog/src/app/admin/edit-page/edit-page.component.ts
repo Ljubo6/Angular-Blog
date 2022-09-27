1d0c6f4789ca16292118e141a5aad0618e33cce3
Lyubomir Lyubomirov <Ljubo6@abv.bg>
2022-09-27 23:12:46 +0300

    edit post page

--- Angular-Blog/src/app/admin/edit-page/edit-page.component.ts
+++ Angular-Blog/src/app/admin/edit-page/edit-page.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, Params} from "@angular/router";
+import {PostsService} from "../../shared/posts.service";
+import {switchMap} from "rxjs";
+import {Post} from "../../shared/interfaces";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-edit-page',
@@ -6,10 +11,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-page.component.scss']
 })
 export class EditPageComponent implements OnInit {
-
-  constructor() { }
+  form!: FormGroup
+  constructor(
+    private route:ActivatedRoute,
+    private postService: PostsService
+    ) { }
 
   ngOnInit(): void {
+    this.route.params.pipe(
+      switchMap((params: Params) => {
+        return this.postService.getById(params['id'])
+      })
+    ).subscribe({
+      next: (post:Post) => {
+        this.form = new FormGroup({
+          title: new FormControl(post.title,Validators.required),
+          text: new FormControl(post.text,Validators.required)
+        })
+      }
+    })
   }
 
+  submit() {
+
+  }
 }
--- Angular-Blog/src/app/shared/posts.service.ts
+++ Angular-Blog/src/app/shared/posts.service.ts
@@ -31,7 +31,18 @@ export class PostsService {
           }))
       }))
   }
+  getById(id:string): Observable<Post>{
+    return this.http.get<Post>(`${environment.fbDbUrl}/posts/${id}.json`)
+      .pipe(map((post: any) => {
+        return{
+          ...post,
+          id,
+          date : new Date(post.date)
+        }
+      }))
+  }
   remove(id:string):Observable<void>{
     return this.http.delete<void>(`${environment.fbDbUrl}/posts/${id}.json`)
+
   }
 }
