5f310f518ee890eeea23142260e9428dfc57aa5a
Lyubomir Lyubomirov <Ljubo6@abv.bg>
2022-09-29 23:24:19 +0300

    pwa

--- Angular-Blog/src/app/post-page/post-page.component.ts
+++ Angular-Blog/src/app/post-page/post-page.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, Params} from "@angular/router";
+import {PostsService} from "../shared/posts.service";
+import {Observable, switchMap} from "rxjs";
+import {Post} from "../shared/interfaces";
 
 @Component({
   selector: 'app-post-page',
@@ -6,10 +10,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./post-page.component.scss']
 })
 export class PostPageComponent implements OnInit {
-
-  constructor() { }
+  post$!: Observable<Post>
+  constructor(
+    private route: ActivatedRoute,
+    private postService: PostsService
+  ) { }
 
   ngOnInit(): void {
+    this.post$ = this.route.params
+      .pipe(switchMap((params:Params) => {
+        return this.postService.getById(params['id'])
+      }))
   }
 
 }
